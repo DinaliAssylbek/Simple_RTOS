1077bac3f3c9c6b03a3767996cc738ec47b27e69
DinaliAssylbek <dinali.assylbek.2003@gmail.com>
2026-04-18 17:20:58 -0400

    Starting OS and defined hardware communication

--- Src/main.c
+++ Src/main.c
@@ -8,40 +8,74 @@
 
 #include <stdint.h>
 #include "stm32f103xb.h"
+#include "rtos.h"
+
+void Profile_Init(void);
+void task0(void);
+void task1(void);
+void task2(void);
 
 int main(void)
 {
-    // Enable GPIOC clock
-    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+	OS_Init();
+	Profile_Init();
+	OS_AddThreads(&task0, &task1, &task2);
+	OS_Launch(8000); // Switch every 1ms
 
-    // Configure PC13 as output (push-pull, 2 MHz)
-    GPIOA->CRL &= ~(0xF << 20);
-    GPIOA->CRL |=  (0x2 << 20);
+	StartOS();
+	return 0;
+}
 
-    while (1) {
-    	GPIOA->ODR ^= (1 << 5); // FORCE HIGH
-    	for (int i = 0; i < 25000; i++);
-    }
+void Profile_Init(void) {
+	   // Enable GPIOA clock
+	    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+	    RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
+	    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+
+	    // Reset Function Mode for PA5, PB2, PC4
+	    GPIOA->CRL &= ~(0xF << 20);
+	    GPIOB->CRL &= ~(0xF << 8);
+	    GPIOC->CRL &= ~(0xF << 16);
+
+		// Configure PA5, PB2, PC4 as output (2 MHz, push-pull)
+	    GPIOA->CRL |=  (0x2 << 20);
+	    GPIOB->CRL |=  (0x2 << 8);
+	    GPIOC->CRL |=  (0x2 << 16);
+}
+
+void Profile_Toggle0() {
+	GPIOA->ODR ^= (1 << 5);
+}
+
+void Profile_Toggle1() {
+	GPIOB->ODR ^= (1 << 2);
+}
+
+void Profile_Toggle2() {
+	GPIOC->ODR ^= (1 << 4);
 }
 
 void task0(void) {
-	int count = 0;
-	while (true) {
-		count++;
+	int count0 = 0;
+	while (1) {
+		count0++;
+		Profile_Toggle0();
 	}
 }
 
 void task1(void) {
-	int count = 0;
-	while (true) {
-		count++;
+	int count1 = 0;
+	while (1) {
+		count1++;
+		Profile_Toggle1();
 	}
 }
 
 void task2(void) {
-	int count = 0;
-	while (true) {
-		count++;
+	int count2 = 0;
+	while (1) {
+		count2++;
+		Profile_Toggle2();
 	}
 }
 
