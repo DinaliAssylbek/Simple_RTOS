3bc173c9cd0bae502223fbd47ac7b923f909e980
DinaliAssylbek <dinali.assylbek.2003@gmail.com>
2026-04-26 02:15:45 -0400

    Cleaned up assembly file

--- Src/rtos_asm.s
+++ Src/rtos_asm.s
@@ -17,12 +17,15 @@ StartOS:
 	LDR R0, =RunPt	 // Get Address of the RunPtr Pointer
 	LDR R1, [R0]	 // Get the Address of TCB (Pointing to the Stack Pointer)
 	LDR SP, [R1]	 // Load Stack Pointer value into SP
+
 	POP {R4-R11}	 // Pop PendSV registers
 	POP {R0-R3}		 // Pop Registers R0 through R3
 	POP {R12}		 // Pop R12 Register
+
 	ADD SP, SP, #4	 // Skip LR Register value
 	POP {LR}		 // Put function address (PC) into LR
 	ADD SP, SP, #4	 // Skip PCR register
+
 	CPSIE I			 // Enable Interrupts
 	BX LR			 // Return to first function (task0)
 
