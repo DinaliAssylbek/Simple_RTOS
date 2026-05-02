00213adefed9af60009c594c22a2d38005aa4074
DinaliAssylbek <dinali.assylbek.2003@gmail.com>
2026-05-02 19:05:23 -0400

    Finished OS timer function implementation

--- Inc/timer.h
+++ Inc/timer.h
@@ -8,6 +8,16 @@
 #ifndef TIMER_H_
 #define TIMER_H_
 
+#include "stm32f1xx_hal.h"
 
+#define TIM2_IRQn_Preempt_Priority 0
+#define TIM2_IRQn_Sub_Priority 0
+#define TIM2_IRQn_Period 1000
+#define TIM2_IRQn_Prescaler 72
+
+void OS_Timer_Init(void);
+void OS_Timer_Start(void);
+void OS_Timer_Reset(void);
+void OS_Timer_ClearITFlag(void);
 
 #endif /* TIMER_H_ */
--- Src/timer.c
+++ Src/timer.c
@@ -5,16 +5,33 @@
  *      Author: dinaliassylbek
  */
 
-
-#include "stm32f1xx_hal.h"
+#include "timer.h"
 
 static TIM_HandleTypeDef TIM_HandleInstance;
 
-void Timer_Init(void) {
+void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim) {
+	__HAL_RCC_TIM2_CLK_ENABLE();
+	HAL_NVIC_SetPriority(TIM2_IRQn, TIM2_IRQn_Preempt_Priority, TIM2_IRQn_Sub_Priority);
+	HAL_NVIC_EnableIRQ(TIM2_IRQn);
+}
+
+void OS_Timer_Init(void) {
 	TIM_HandleInstance.Instance = TIM2;
-	TIM_HandleInstance.Init.Prescaler = 71; // 1 MHz
+	TIM_HandleInstance.Init.Prescaler = TIM2_IRQn_Prescaler - 1;
 	TIM_HandleInstance.Init.CounterMode = TIM_COUNTERMODE_DOWN;
-	TIM_HandleInstance.Init.Period = 999;
+	TIM_HandleInstance.Init.Period = TIM2_IRQn_Period - 1;
 	TIM_HandleInstance.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
 	HAL_TIM_Base_Init(&TIM_HandleInstance);
+}
+
+void OS_Timer_Start(void) {
+	HAL_TIM_Base_Start_IT(&TIM_HandleInstance);
+}
+
+void OS_Timer_Reset(void) {
+	__HAL_TIM_SET_COUNTER(&TIM_HandleInstance, 0);
+}
+
+void OS_Timer_ClearITFlag(void) {
+	__HAL_TIM_CLEAR_IT(&TIM_HandleInstance, TIM_IT_UPDATE);
 }
